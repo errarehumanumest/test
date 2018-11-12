45941ff40cca7b906377013e0f6338cf64c3a41b
Antanas <antanas@antanass-macbook-pro.local>
2018-11-12 19:45:26 +0200

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Antanas on 12/11/2018.
+//  Copyright © 2018 Antanas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
